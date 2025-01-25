85d2808c16da75e017d95a2c704407c9b860bf44
maxsanttos <maxsanttos@hotmail.com>
2025-01-25 12:41:02 -0300

    uodate class controller order and user, add @PathVariable

--- src/main/java/com/course/project/controller/OrderController.java
+++ src/main/java/com/course/project/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class OrderController {
     public ResponseEntity<List<Order>> findAll() {
         List<Order> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        Order obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/course/project/controller/UserController.java
+++ src/main/java/com/course/project/controller/UserController.java
@@ -5,6 +5,7 @@ import com.course.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(Long id) {
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
