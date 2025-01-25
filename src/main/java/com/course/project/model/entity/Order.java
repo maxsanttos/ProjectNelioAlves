cff04db2fb209b73446f703f054c67b59f4c47c1
maxsanttos <maxsanttos@hotmail.com>
2025-01-25 14:48:51 -0300

    updates

--- src/main/java/com/course/project/model/entity/Order.java
+++ src/main/java/com/course/project/model/entity/Order.java
@@ -1,12 +1,15 @@
 package com.course.project.model.entity;
 
 import com.course.project.model.entity.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "orders")
@@ -17,12 +20,17 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:MM:mm:ss'Z'",timezone = "UTC")
     private Instant moment;
+
     private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> orderItems= new HashSet<>();
 
     public Order() {}
 
@@ -64,6 +72,10 @@ public class Order implements Serializable {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Set<OrderItem> getOrderItems() {
+        return orderItems;
     }
 
     @Override
