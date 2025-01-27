55e45b758256762cc4ca95c8351b3da28f6f8682
maxsanttos <maxsanttos@hotmail.com>
2025-01-27 16:15:15 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/course/project/controller/OrderController.java
+++ src/main/java/com/course/project/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.course.project.controller;
 import com.course.project.model.entity.Order;
 import com.course.project.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/course/project/model/entity/Product.java
+++ src/main/java/com/course/project/model/entity/Product.java
@@ -8,6 +8,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "products")
 public class Product implements Serializable {
@@ -28,6 +30,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -81,6 +86,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
