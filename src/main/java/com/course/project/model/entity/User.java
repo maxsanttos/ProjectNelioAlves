756e79153332c9e3039bd08c829f52a48adfc48d
maxsanttos <maxsanttos@hotmail.com>
2025-01-29 13:09:16 -0300

    Updates

--- src/main/java/com/course/project/model/entity/User.java
+++ src/main/java/com/course/project/model/entity/User.java
@@ -37,4 +37,12 @@ public class User  implements Serializable {
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
 }
