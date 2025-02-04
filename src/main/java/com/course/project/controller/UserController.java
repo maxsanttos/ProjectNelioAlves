3441483cae77763bd1a27406e7ce035306a5b126
maxsanttos <maxsanttos@hotmail.com>
2025-02-04 15:27:30 -0300

    User update

--- src/main/java/com/course/project/controller/UserController.java
+++ src/main/java/com/course/project/controller/UserController.java
@@ -44,4 +44,9 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
 }
